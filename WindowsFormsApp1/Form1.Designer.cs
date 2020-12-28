fe690b10e209b24edaac903000eb5bb3067d0e74
chunxina <chunxi1@qq.com>
2020-12-28 17:15:59 +0800

    添加项目文件。

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("更纱黑体 SC", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(83, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(146, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("更纱黑体 SC", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox2.Location = new System.Drawing.Point(83, 99);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(146, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "转换结果";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("更纱黑体 SC", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(117, 143);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "转换";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("更纱黑体 SC", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(83, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "输入 RGB 色值，逗号分隔";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(311, 207);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RGB转16进制色值";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string color = textBox1.Text;
+            try
+            {
+                string[] a;
+                if (color.Contains(","))
+                {
+                    a = color.Split(',');
+                }
+                else
+                {
+                    a = color.Split('，');
+                };
+                int x = Convert.ToInt16(a[0]);
+                int y = Convert.ToInt16(a[1]);
+                int z = Convert.ToInt16(a[2]);
+                string b;
+                b = String.Format("#{0:X}{1:X}{2:X}", x, y, z);
+                if (x <= 15 && y <= 15 && z <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}{4:X}{5:X}", x, x, y, y, z, z);
+                }
+                else if (x <= 15 && z <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}{4:X}", x, x, y, z, z);
+                }
+                else if (y <= 15 && z <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}{4:X}", x, y, y, z, z);
+                }
+                else if (x <= 15 && y <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}{4:X}", x, x, y, y, z);
+                }
+                else if (y <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}", x, y, y, z);
+                }
+                else if (z <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}", x, y, z, z);
+                }
+                else if (x <= 15)
+                {
+                    b = String.Format("#{0:X}{1:X}{2:X}{3:X}", x, x, y, z);
+                }
+                textBox2.Text = b;
+            }
+            catch (Exception)
+            {
+                textBox2.Text = "输入错误";
+            }
+        }
+    }
+}
